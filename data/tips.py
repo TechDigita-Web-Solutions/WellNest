7e507e3f1eda43dac123477852e92cbb5d0e84ef
saathvik-sheerla <saathvik.sheerla@gmail.com>
2024-11-15 17:36:24 +0530

    added tipspage

---
+++ data/tips.py
@@ -0,0 +1,6 @@
+tips = [
+    {'title': 'diet', 'img': 'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/vegetarian-diet-plan-1296x728-feature.jpg?w=1155&h=1528'},
+    {'title': 'yoga', 'img': 'https://cdn.pixabay.com/photo/2024/06/21/07/46/yoga-8843808_1280.jpg'},
+    {'title': 'routine', 'img': 'https://m.media-amazon.com/images/I/61fUz-XMD6L._AC_UF1000,1000_QL80_.jpg'},
+    {'title': 'travel', 'img': 'https://static.vecteezy.com/system/resources/thumbnails/008/063/100/small_2x/rear-view-portrait-of-young-man-traveler-with-backpack-standing-on-a-mountain-with-arms-spread-open-travel-life-style-and-adventure-concept-free-photo.jpg'},
+]
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from modules.user_db_insert import insert_user
 from modules.user_auth import authenticate_user
 from modules.auth import auth_bp
 import sqlite3
+from data.tips import tips as tipsdata
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
@@ -18,7 +19,8 @@ def tests():
 
 @app.route('/tips')
 def tips():
-    return render_template('tips.html')
+    print(tipsdata)
+    return render_template('tips.html', tipsdata=tipsdata)
 
 @app.route('/profile')
 def profile():
